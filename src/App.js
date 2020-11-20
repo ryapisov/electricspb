7190525e0d2ae047adb6b7d642c5fd0a135d7279
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-11-20 12:57:07 +0300

    Added grid

--- src/App.js
+++ src/App.js
@@ -1,273 +1,46 @@
-import video from './videos/720.mp4'
-import music from './music/ppk.mp3'
-import './style.scss'
-
+import React from 'react'
 import styled from 'styled-components'
+import MainSection from './components/sections/MainSection'
+import BodySection from './components/sections/BodySection'
+import FooterSection from './components/sections/FooterSection'
 
-import 'normalize.css'
+const Wrap = styled.main`
+  display:flex;
+  flex-direction:column;
+`
 
-const Wrap = styled.div`
+const Main = styled.section`
+  background: #591;
+  position:relative;
+  height:100vh;
+`
+
+const Body = styled.section`
+  background: #155;
+  position:relative;
+  height:100vh;
+`
+
+const Footer = styled.section`
+  background: #525;
   position:relative;
   height:100px;
-
-`
-const Mask = styled.div`
-  &:after{
-    content:'';
-    height:650px;
-    width:100%;
-    display:block;
-    position:absolute;
-    background: rgba(10, 10,50, .2);
-    top:0;
-    left:0;
-    z-index:2;
-  }
-`
-const Video = styled.video`
-  position:absolute;
-  width:100%;
-  height:auto; 
-  top:0;
-  left:0;
-  z-index:1;
-`
-
-const Text = styled.div`
-  position:absolute;
-  width:650px;
-  height:100%;
-  margin-top:540px;
-  margin-left:70px;
-  left:0;
-  top:0;
-  color:#fff;
-  font-family: 'Russo One', sans-serif;
-  -webkit-text-stroke: 1.5px black;
-  opacity:0.8;
-  font-size:75px;
-  z-index:5;
-  -webkit-user-select: none;
-
-
-`
-const TextRight = styled.div`
-  position:absolute;
-  width:330px;
-  height:100%;
-  color:#fff;
-  font-family: 'Russo One', sans-serif;
-  -webkit-text-stroke: 1px black;
-  font-size:32px;
-  top:30px;
-  right:110px;
-  z-index:5;
-  padding:25px;
-  -webkit-user-select: none;
-
-
-
-`
-
-const Name = styled.div`
-  position:absolute;
-  width:650px;
-  height:50px;
-  color:#fff;
-  font-family: 'Russo One', sans-serif;
-  -webkit-text-stroke: 0.6px black;
-  font-size:32px;
-  opacity:0.8;
-  top:490px;
-  left:75px;
-  z-index:5;
-  -webkit-user-select: none;
-
-
-`
-
-const Info = styled.div`
-  position:absolute;
-  width:501px;
-  height:100%;
-  color:#fff;
-  font-family: 'Russo One', sans-serif;
-  -webkit-text-stroke: 1px black;
-  font-size:28px;
-  top:350px;
-  right:0px;
-  z-index:5;
-  padding:25px;
-  -webkit-user-select: none;
-  background-color: rgba(0, 0, 30, 0.2);
-  border-radius:4%;
-
-`
-
-const BtnVideo = styled.button`
-  position:absolute;
-  top:240px;
-  left:285px;
-  width: 250px;
-  height: 80px;
-  border-radius:4%;
-  border:0px;
-  text-decoration: none;
-  color: #999;
-  text-align: center;
-  font-family: 'Russo One', sans-serif;
-  font-size: 26px;
-  z-index:5;
-  &:hover{
-    cursor:pointer;
-    background-color:#ff4d00;
-    color: #fff;
-  }
-
-`
-
-const Page = styled.div`
-  position:relative;
-  height: 200vh;
-  background: rgba(0, 0, 30, 0.7);
-  color: #fff;
-  
-`
-const PageText = styled.div`
-  position:absolute;
-  left:0;
-  top:900px;
-  font-family: 'Russo One', sans-serif;
-`
-
-const Online = styled.div`
-  position:absolute;
-  display:none;
-  left:0px;
-  top:100px;
-  width:100%;
-  height:420px;
-  background-color: rgba(0, 0, 0, 0.9);
-  border-radius:1%;
-  z-index:6;
-  text-align:center;
-  padding-top:80px;
-`
-
-const Guarantee = styled.div`
-  position:absolute;
-  width:300px;
-  height:30px;
-  color:#fff;
-  font-family: 'Russo One', sans-serif;
-  -webkit-text-stroke: 1px black;
-  font-size:32px;
-  top:30px;
-  left:350px;
-  z-index:5;
-  padding:25px;
-  -webkit-user-select: none;
-  background-color: rgba(0, 0, 30, 0.2);
-  border-radius:4%;
-`
-
-const Footer = styled.div`
-  position:relative;
-  padding-top:20px;
-  height: 10vh;
-  font-family: 'Russo One', sans-serif;
-  background: rgba(255, 255, 255, 0.7);
-  color: #999;
-  text-align:center;
-  -webkit-text-stroke: 1px black;
-  font-size:16px;
 `
 
 function App() {
   return (
-    <>
-      <Wrap>
-      <Name>Иванов Александр </Name>
-      <Guarantee>Гарантия от 5 лет</Guarantee> 
-      <Text>
-      
-        ЭЛЕКТРИК  СПБ
-        
-        </Text>
-      <TextRight>
-        +7 965 046-59-39
-        gremm87@yandex.ru
-
-
-      </TextRight>
-      <BtnVideo>Видео работ</BtnVideo>    <iframe  style={{display:'inline-block'}}
-          width="560" 
-          height="315" 
-        src="https://www.youtube.com/embed/rb4CYrD9T_o" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-       &nbsp;  &nbsp;  &nbsp;
-       <iframe  style={{display:'inline-block'}}
-          width="560" 
-          height="315" 
-        src="https://www.youtube.com/embed/rb4CYrD9T_o" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-
-      <Info>
-        Электромонтажные работы в квартирах
-        домах, офисах.
-        lorem var bus goblin 
-        <audio src={music} controls></audio>
-      </Info>
-    
-
-      <Mask>
-        <Video  src={video} autoPlay muted loop></Video>
-        <Online>
-        <iframe  style={{display:'inline-block'}}
-          width="560" 
-          height="315" 
-        src="https://www.youtube.com/embed/rb4CYrD9T_o" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-       &nbsp;  &nbsp;  &nbsp;
-       <iframe  style={{display:'inline-block'}}
-          width="560" 
-          height="315" 
-        src="https://www.youtube.com/embed/rb4CYrD9T_o" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-
-       
-        </Online>
-      </Mask>
-      </Wrap>
-    <Page>
-      <PageText>
-      Реферат по математике
-Тема: «Возрастающий минимум: основные моменты»
-Первая производная, очевидно, положительна. Дифференциальное уравнение развивает невероятный ортогональный определитель, явно демонстрируя всю чушь вышесказанного. Не доказано, что геодезическая линия последовательно раскручивает определитель системы линейных уравнений. Интеграл по бесконечной области вполне вероятен. Натуральный логарифм специфицирует полином. Замкнутое множество поддерживает двойной интеграл.
-
-Наибольшее и наименьшее значения функции традиционно концентрирует отрицательный ортогональный определитель. Собственное подмножество, не вдаваясь в подробности, стабилизирует предел последовательности. Функция многих переменных ускоряет определитель системы линейных уравнений. Ряд Тейлора, следовательно, специфицирует ротор векторного поля. Вектор охватывает степенной ряд, откуда следует доказываемое равенство.
-
-Постоянная величина концентрирует многочлен. Уравнение в частных производных непосредственно уравновешивает бином Ньютона, откуда следует доказываемое равенство. Наибольший Общий Делитель (НОД), в первом приближении, обуславливает коллинеарный Наибольший Общий Делитель (НОД). Сумма ряда, исключая очевидный случай, развивает определитель системы линейных уравнений.
-
-
-      </PageText>
-    </Page>
-
-    <Footer>
-      Санкт-Петербург 2020 <br/><br/>
-      +7 965 046-59-39
-    </Footer>
-
-    
-
-  </>
+    <Wrap>
+      <Main>
+        <MainSection />
+      </Main>
+      <Body>
+        <BodySection />
+      </Body>
+      <Footer>
+        <FooterSection />
+      </Footer>
+    </Wrap>
   )
 }
 
-export default App;
+export default App
