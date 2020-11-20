c6f36b677a76ce26505e10f46b213f2f3cf23fb9
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-11-20 22:40:15 +0300

    Сhanging the file structure

--- src/sections/MainSection.js
+++ src/sections/MainSection.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import VideoBanner from '../components/VideoBanner/Video'
-import TextBlock from '../components/TextBlock/TextBlock'
+import { MainTitle, Phone, Email, Info, Name, BigButton } from '../components'
 
 export default ()=>{
 
   return (
-    <>1abc
-      <TextBlock w={'444'} h="300" r="6" size="24" top="33" left="22" >
-        электрик
-      </TextBlock>
+    <>
       <VideoBanner />
+
+      <MainTitle size="3" padding="0.3" opacity="1" t="50" l="5">
+        ЭЛЕКТРИК СПБ
+      </MainTitle>
+
+      <Name size="1" padding="0.5"  t="43" l="10">
+         Александр Иванов
+      </Name>
+
+      <Info size="1" padding="0.5" opacity="4" t="67" l="30">
+         Электромонтажные работы в квартирах, домах, офисах.
+      </Info>
+
+      <BigButton size="1" padding="0.5" opacity="3" t="20" l="100">
+        Видео работ
+      </BigButton>
+
+      <Phone size="1" padding="0.5" opacity="3" t="8" l="100">
+        +7 965 046-59-39
+      </Phone>
+
+      <Email size="1" padding="0.5" opacity="3" t="20" l="100">
+        gremm87@yandex.ru
+      </Email>
+      
     </>
   )
 }
